85d2e19f4b0f403ea40b092da4c8c92c4baa995a
alepmaros <alehstk@gmail.com>
2018-10-11 14:43:18 -0300

    bovw

---
+++ src/bovw.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import pandas as pd
+import os
+
+
+image_csv = pd.read_csv('datasets/photonet/photonet_dataset_cleaned.csv').sample(50)
+image_csv2 = pd.read_csv('datasets/photonet/photonet_dataset_cleaned.csv').sample(50)
+
+# Create Sift Extractor
+sift = cv2.xfeatures2d.SIFT_create()
+
+dictionarySize = 32
+
+BOW = cv2.BOWKMeansTrainer(dictionarySize)
+
+for _, row in image_csv.iterrows():
+    p_img = 'datasets/photonet/imgs/{}.jpg'.format(row['photo_id'])
+    try:
+        print(p_img)
+        image = cv2.imread(p_img)
+        # gray = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+        kp, dsc= sift.detectAndCompute(image, None)
+        BOW.add(dsc)
+    except Exception as e:
+        print('Failed to use image {}: {}'.format(p_img, e))
+
+#dictionary created
+sift2 = cv2.xfeatures2d.SIFT_create()
+BOW_descriptor = cv2.BOWImgDescriptorExtractor(sift2, cv2.BFMatcher(cv2.NORM_L2))
+BOW_descriptor.setVocabulary(BOW.cluster())
+
+
+### GET THE DESCRIPTORS
+for _, row in image_csv2.iterrows():
+    p_img = 'datasets/photonet/imgs/{}.jpg'.format(row['photo_id'])
+    
+    try:
+        print(p_img)
+        image = cv2.imread(p_img)
+        # gray = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+        kp, dsc= sift.detectAndCompute(image, None)
+        print(BOW_descriptor.compute(image, kp))
+    except Exception as e:
+        print('Failed to use image {}: {}'.format(p_img, e))
+
+
