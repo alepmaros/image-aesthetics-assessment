3b459ea60debe591be494450ce54ddfcff3bdbdb
alepmaros <alehstk@gmail.com>
2018-10-30 17:37:22 -0200

    More models

---
+++ src/analysis/plot_distribution.py
@@ -0,0 +1,44 @@
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+
+SEED = 481516
+imgs_csv = pd.read_csv('datasets/photonet/photonet_dataset_cleaned.csv')
+
+imgs_train, imgs_test = train_test_split(imgs_csv, test_size=0.2, random_state=SEED)
+imgs_test, imgs_cv = train_test_split(imgs_test, test_size=0.5, random_state=SEED)
+
+with plt.style.context(('seaborn-darkgrid')):
+    f = plt.figure()
+    
+    plt.title('Distribuição das médias das notas - Dataset Completo')
+    plt.ylabel('Quantidade')
+    plt.xlabel('Nota')
+    plt.xlim(2,7)
+    plt.hist(imgs_csv['mean_ratings'].values, bins=25, histtype='stepfilled')
+    plt.show()
+
+    
+
+with plt.style.context(('seaborn-darkgrid')):
+    f = plt.figure()
+    
+    plt.title('Distribuição das médias das notas - Dataset Treinamento')
+    plt.ylabel('Quantidade')
+    plt.xlabel('Nota')
+    plt.xlim(2,7)
+    plt.hist(imgs_train['mean_ratings'].values, bins=25, histtype='stepfilled')
+    plt.show()
+
+with plt.style.context(('seaborn-darkgrid')):
+    f = plt.figure()
+    
+    plt.title('Distribuição das médias das notas - Dataset Teste')
+    plt.ylabel('Quantidade')
+    plt.xlabel('Nota')
+    plt.xlim(2,7)
+    plt.hist(imgs_test['mean_ratings'].values, bins=25, histtype='stepfilled')
+    plt.show()
--- src/train_model.py
+++ src/train_model.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
-from keras.callbacks import ModelCheckpoint, Callback, EarlyStopping
+from keras.callbacks import ModelCheckpoint, Callback, EarlyStopping, TensorBoard
 
 from models.models import get_proposed_model, LossHistory
 from utils.data_generators import train_generator, valid_generator, _SIZE_CV, _SIZE_TRAIN
@@ -19,7 +19,9 @@ history = LossHistory()
 early_stopping = EarlyStopping(monitor='val_loss', patience=5, verbose=1, mode='auto')
 checkpoint = ModelCheckpoint('proposed_model.h5', monitor='val_loss', verbose=1, save_best_only=True,
                              mode='min')
-callbacks = [history,  checkpoint]
+tensorboard = TensorBoard(log_dir='Graph', histogram_freq=0, write_graph=True,
+                        write_images=True)
+callbacks = [history,  checkpoint, tensorboard]
 
 model.fit_generator(
     train_generator(BATCH_SIZE),
